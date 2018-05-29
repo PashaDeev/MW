5c95511c79fd3b352c9c0530b727efe6637925b7
WD-man <boxofwd@gmail.com>
2018-05-29 18:36:50 +0300

    timline с движением

--- js/controller.js
+++ js/controller.js
@@ -10,17 +10,31 @@ export default class controller {
   }
 
   blocksInit() {
-    this.blokArr = [];
+    this.blockArr = [];
     let count = 0;
     for (let item of this.blocks) {
-      this.blokArr[count] = new Parallax(item, this.model[count].duration, this.model[count]);
+      this.blockArr[count] = new Parallax(item, this.model[count].duration, this.model[count]);
       ++count;
     }
   }
 
   timelineInit() {
+
+    let timelineCoords = [];
+    for (let item of this.blockArr) {
+      if (item.block.classList.contains(`timeline-block`)) {
+        timelineCoords.push(item._startParallax);
+      }
+    }
+
+    let fixed = document.querySelector(`.collect-six > .collection__container`).getBoundingClientRect();
+    fixed = {
+      top: this.blockArr[this.blockArr.length - 1]._endParallax - this.blockArr[3]._startParallax,
+      left: fixed.left
+    };
+
     ReactDom.render(
-      <Timeline/>,
+      <Timeline coords={timelineCoords} fixed={fixed}/>,
       document.getElementById(`timeline`)
     );
   }
@@ -30,7 +44,7 @@ export default class controller {
     this.timelineInit();
 
     document.addEventListener(`scroll`, () => {
-      for (let item of this.blokArr) {
+      for (let item of this.blockArr) {
         if (window.pageYOffset > item._startParallax) {
 
           if (window.pageYOffset > item._startParallax && window.pageYOffset < item._endParallax) {
--- js/timeline.js
+++ js/timeline.js
@@ -12,16 +12,94 @@ class TimelineItem extends React.Component {
 }
 
 export default class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.blockCoords = props.coords;
+    this.fixed = props.fixed;
+    this.state = {
+      currentBlock: 0,
+      position: `absolute`,
+      top: 0,
+      left: 0,
+    };
+    this.onDocScroll = this.onDocScroll.bind(this);
+  }
+
+  onDocScroll() {
+
+    if (window.pageYOffset < this.blockCoords[0]) {
+      this.setState({
+        currentBlock: 0,
+        position: `absolute`,
+        top: 0,
+        left: 0
+      });
+    } else if (this.blockCoords[0] < window.pageYOffset && window.pageYOffset < this.blockCoords[1]) {
+      this.setState({
+        currentBlock: 0,
+        position: `fixed`,
+        top: 117,
+        left: this.fixed.left
+      });
+    } else if (this.blockCoords[1] < window.pageYOffset && window.pageYOffset < this.blockCoords[2]) {
+      this.setState({
+        currentBlock: 1,
+        position: `fixed`,
+        top: 117,
+        left: this.fixed.left
+      });
+    } else if (this.blockCoords[2] < window.pageYOffset && window.pageYOffset < this.blockCoords[3]) {
+      this.setState({
+        currentBlock: 2,
+        position: `fixed`,
+        top: 117,
+        left: this.fixed.left
+      });
+    } else if (this.blockCoords[3] < window.pageYOffset && window.pageYOffset < this.blockCoords[4]) {
+      this.setState({
+        currentBlock: 3,
+        position: `fixed`,
+        top: 117,
+        left: this.fixed.left
+      });
+    } else if (this.blockCoords[4] < window.pageYOffset) {
+      this.setState({
+        currentBlock: 4,
+        position: `fixed`,
+        top: 117,
+        left: this.fixed.left
+      });
+    }
+  }
+
+  componentDidMount() {
+
+    document.addEventListener(`scroll`, this.onDocScroll);
+
+  }
+
+  componentWillUnmount() {
+
+    document.removeEventListener(`scroll`, this.onDocScroll);
+
+  }
+
   render() {
-    return (<ul className="timeline">
-      {timelineElements.map((item, index) => {
-        if (index === 0) {
-          return (<TimelineItem key={item.disabled.toString()} name={item.active}/>);
-        } else {
-          return (<TimelineItem key={item.disabled.toString()} name={item.disabled}/>);
+    return (
+      <ul className="timeline" style={{position: this.state.position, top: this.state.top, left: this.state.left}}>
+        {timelineElements.map((item, index) => {
+          if (index === this.state.currentBlock) {
+            return (
+              <TimelineItem key={item.disabled.toString()} name={item.active}/>
+            );
+          } else {
+            return (
+              <TimelineItem key={item.disabled.toString()} name={item.disabled}/>
+            );
+          }
+        })
         }
-      })
-      }
-    </ul>);
+      </ul>
+    );
   }
 }
