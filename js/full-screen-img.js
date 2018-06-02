e95a176074b85edfc10649ffa00ab5943193a33b
WD-man <boxofwd@gmail.com>
2018-06-02 18:50:37 +0300

    работает потому что не работает

--- js/full-screen-img.js
+++ js/full-screen-img.js
@@ -1,15 +1,25 @@
-import imgMove from './img-move';
+import ImgMove from './img-move';
+
+function onPressEsc(event) {
+  if (event.keyCode === 27) {
+    document.querySelector(`.full-screen`).classList.remove(`full-screen`);
+
+    document.removeEventListener(`keydown`, onPressEsc);
+  }
+}
 
 export default function fullScreenImg() {
   document.addEventListener(`click`, (e) => {
     if (e.target.classList.contains(`moved-image`)) {
       e.target.classList.toggle(`full-screen`);
-      e.init = imgMove.move();
+      e.init = ImgMove.move();
 
 
       if (e.target.classList.contains(`full-screen`)) {
         e.target.style.transform = ``;
         e.target.style.clipPath = ``;
+
+        document.addEventListener(`keydown`, onPressEsc);
       } else {
         e.init();
       }
